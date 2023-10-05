8b7f4942286d4f22e476e7017a723322e8d2fac7
Emre Ezim <emreezim@hotmail.com>
2023-10-05 23:11:24 +0300

    Yüz Tanıma Desteği Basit Hali İle

--- SesliSohbetPython/SesliSohbetAsistanı.py
+++ SesliSohbetPython/SesliSohbetAsistanı.py
@@ -1,7 +1,7 @@
 import random
 import time
 import fnmatch
-
+import cv2
 from playsound import playsound
 import speech_recognition as sr
 from gtts import gTTS
@@ -56,7 +56,41 @@ class sesliasistan():
 
 
 
+    def faceDef(self):
 
+        vid = cv2.VideoCapture(0)
+        yuz_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+        img = cv2.imread(r"WIN_20231005_22_04_35_Pro.jpg")
+        x = 0
+        y = 0
+        w = 0
+        h = 0
+
+        while (True):
+            ret, frame = vid.read()
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            gray2 = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            yuzler2 = yuz_cascade.detectMultiScale(gray2, 1.3, 5)
+            yuzler = yuz_cascade.detectMultiScale(gray, 1.3, 5)
+
+            print(yuzler)
+            # x   y   w   h
+            # 225  67 196 196
+            # 187 142 212 212
+
+            for (x, y, w, h) in yuzler:
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (85, 255, 0), 3)
+
+            if x == 200 in yuzler:
+                print("sadsad")
+                break
+
+            cv2.imshow('title', frame)
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        vid.release()
+        cv2.destroyAllWindows()
 
     def ses_karisilik(self,gelen_ses):
         if "selam" in gelen_ses:
@@ -66,7 +100,10 @@ class sesliasistan():
         elif "nasılsın" in gelen_ses:
             self.seslendirme("iyiyim siz nasılsınız")
         elif "nasıl gidiyor" in gelen_ses:
-            self.seslendirme("iyi sizin?")    
+            self.seslendirme("iyi sizin?")
+        elif "tamam" in gelen_ses:
+            print("asdsadsa")
+            self.faceDef()
         elif "video aç" in gelen_ses:
             self.seslendirme("Ne açmamı istersiniz")
             veri=self.ses_kayit()
